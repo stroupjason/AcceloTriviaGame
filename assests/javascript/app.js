c5475a2d448aea7d7ae6f04e148d297c5b6fc835
Jason Stroup <jasonstroup12@gmail.com>
2019-07-18 04:32:20 -0600

    almost done

--- assests/javascript/app.js
+++ assests/javascript/app.js
@@ -57,7 +57,7 @@ var questions = [{
         answers: ["Quotes Module", "Project, Tickets, and Retainers", "Project, Request, Sales", "Invoice Module"],
         correctAnswer: "triggers"
     }
-]
+];
 
 //Variable that will hold the setInterval
 var timer;
@@ -69,7 +69,7 @@ var game = {
 
     countdown: function() {
         game.counter--;
-        $("#counter-number").text(game.counter);
+        $("#counter-number").html(game.counter);
         if (game.counter === 0) {
             console.log("TIME UP");
             game.done();
@@ -77,10 +77,10 @@ var game = {
     },
 
     start: function() {
-        timer = setInterval(game.counter, 1000);
+        timer = setInterval(game.countdown, 1000);
 
         $("#sub-wrapper").prepend(
-            "<h2>Time Remaining: <span id='#counter-number'>180</span>Seconds</h2>"
+            "<h2>Time Remaining: <span id='#counter-number'>180</span> Seconds</h2>"
         );
 
         $("#start").remove();
@@ -91,8 +91,40 @@ var game = {
                 card.append("<input type='radio' name='question-" + i +
                     "' value'" + questions[i].answers[j] + "''>" + questions[i].answers[j]);
             }
-        };
+        }
+        card.append("<button id='done'>Done</button>")
+    },
+
+    //Where we left off on Wed night (1hr demo video via zoom)
+    //checking for the input values to see if the selected radio button is the correctAnswer or not
+    done: function() {
+        var inputs = card.children("input:checked");
+        for (var i = 0; i < inputs.length; i++) {
+            if ($(inputs[i]).val() === questions[i].correctAnswer) {
+                game.correct++;
+            } else {
+                game.incorrect++;
+            }
+        }
+        this.result();
+    },
+
+    //append the final results - score
+    result: function() {
+        clearInterval(timer);
+        $("#sub-wrapper h2").remove();
+
+        card.html("<h2>All Done!</h2>");
+        card.append("<h3>Correct Answers: " + this.correct + "</h3>");
+        card.append("<h3>Incorrect Answers: " + this.incorrect + "</h3>")
     }
+};
 
+// CLICK EVENT HANDLERS
+$(document).on("click", "#start", function() {
+    game.start();
+});
 
-}
+$(document).on("click", "#done", function() {
+    game.start();
+});
